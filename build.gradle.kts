89f30f384bd3777dd84c54992ea19e3d899f3418
Suresh G <sgopal1@walmartlabs.com>
2017-06-11 14:38:33 -0700

    Dokka version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.13")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.14")
     }
 }
 
